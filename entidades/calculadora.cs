e48a769616a89387f1915edd0e1e2259e194f953
matias <ledesma.arispe.matias@hotmail.com>
2021-04-07 21:05:39 -0300

    validar operador terminado

---
+++ entidades/calculadora.cs
@@ -0,0 +1,20 @@
+﻿namespace entidades
+{
+    public static class Calculadora
+    {
+        public static double Operar(Numero numero1, Numero numero2, string operador)
+        {
+
+
+        }
+
+        private static string ValidarOperador(char operador)
+        {
+            if (operador != '+' || operador != '-' || operador != '*' || operador != '/')
+            {
+                return "+";
+            }
+            return operador.ToString();
+        }
+    }
+}
