b9a849df4dc597d4e7c19416d91e49e1d7d3a746
ledesmaarispematias <ledesma.arispe.matias@hotmail.com>
2021-04-09 21:19:59 -0300

    agrege el resumen de las funciones hechas hasta el momento.

--- Trabajo Practico 1/Numero.cs
+++ Trabajo Practico 1/Numero.cs
@@ -5,19 +5,26 @@ namespace Trabajo_Practico_1
     internal class Numero
     {
         private double numero;
-
+        /// <summary>
+        /// El constructor por defecto (sin parámetros) asignará valor 0 al atributo numero.
+        /// </summary>
         public Numero()
         {
             numero = 0;
         }
-
+        /// <summary>
+        /// ValidarNumero comprobará que el valor recibido sea numérico, y lo retornará en
+       /// formato double. Caso contrario, retornará 0.
+        /// </summary>
         private double ValidarNumero(string strNumero)
         {
             double retorno = 0;
             Double.TryParse(strNumero, out retorno);
             return retorno;
         }
-
+        /// <summary>
+        /// La propiedad SetNumero asignará un valor al atributo número, previa validación.
+        /// </summary>
         public string setNumero
         {
             set
@@ -26,7 +33,10 @@ namespace Trabajo_Practico_1
                     numero = ValidarNumero(value);
             }
         }
-
+        /// <summary>
+        /// El método privado EsBinario validará que la cadena de caracteres esté compuesta
+        /// SOLAMENTE por caracteres '0' o '1'.
+        ///  </summary>
         private bool EsBinario(string cadena)
         {
             bool esBinario = true;
