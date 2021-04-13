48a2a80bdab2d43d4af9cd22429d69c1f292dc19
ledesmaarispematias <ledesma.arispe.matias@hotmail.com>
2021-04-13 01:49:54 -0300

    ordenando

---
+++ Trabajo Practico 1/MiCalculadora/Form1.cs
@@ -0,0 +1,72 @@
+﻿using System.Windows.Forms;
+
+namespace Trabajo_Practico_1
+{
+    public partial class Form1 : Form
+    {
+      
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void richTextBox1_TextChanged(object sender, System.EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, System.EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, System.EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, System.EventArgs e)
+        {
+
+        }
+
+        private void treeView1_AfterSelect(object sender, TreeViewEventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, System.EventArgs e)
+        {   
+
+        }
+
+        private void button1_Click(object sender, System.EventArgs e)
+        {
+            MessageBox.Show("boton1");
+        }
+
+        private void button2_Click(object sender, System.EventArgs e)
+        {
+            MessageBox.Show("boton2");
+        }
+        private void button3_Click(object sender, System.EventArgs e)
+        {
+            MessageBox.Show("boton3");
+        }
+        private void button4_Click(object sender, System.EventArgs e)
+        {
+            MessageBox.Show("boton4");
+        }
+
+        private void button5_Click(object sender, System.EventArgs e)
+        {
+            MessageBox.Show("boton5");
+        }
+
+        private void label1_Click(object sender, System.EventArgs e)
+        {
+
+        }
+    }
+}
