ed181637bd1b0f64127a253540fd6f950c6d4052
matias <ledesma.arispe.matias@gmail.com>
2021-05-09 16:34:03 -0300

    agregue el tp2

---
+++ Trabajo Practico 2/Entidades/Sedan.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Text;
+
+namespace Entidades
+{
+    public class Sedan : Vehiculo
+    {
+        public enum ETipo { CuatroPuertas, CincoPuertas }
+
+        private ETipo tipo;
+
+        /// <summary>
+        /// Por defecto, TIPO será CuatroPuertas
+        /// </summary>
+        /// <param name="marca"></param>
+        /// <param name="chasis"></param>
+        /// <param name="color"></param>
+        public Sedan(EMarca marca, string chasis, ConsoleColor color) : base(chasis, marca, color)
+        {
+            tipo = ETipo.CuatroPuertas;
+        }
+
+        /// <summary>
+        /// con set para tipo puertas
+        /// </summary>
+        /// <param name="marca"></param>
+        /// <param name="chasis"></param>
+        /// <param name="color"></param>
+        public Sedan(EMarca marca, string chasis, ConsoleColor color, ETipo puertas) : this(marca, chasis, color)
+        {
+            tipo = puertas;
+        }
+
+        /// <summary>
+        /// Sedan son 'Mediano'
+        /// </summary>
+        protected override ETamanio Tamanio
+        {
+            get
+            {
+                return ETamanio.Mediano;
+            }
+        }
+
+        public override sealed string Mostrar()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine("SEDAN");
+            sb.AppendLine(base.Mostrar());
+            sb.AppendLine("TAMAÑO : " + this.Tamanio.ToString());
+            sb.AppendLine("TIPO : " + this.tipo.ToString());
+            sb.AppendLine();
+            sb.AppendLine("---------------------");
+
+            return sb.ToString();
+        }
+    }
+}
---
+++ consoleForTesting/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Entidades;
+
+namespace consoleForTesting
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Taller nuevoTaller = new Taller(6);
+
+
+        }
+    }
+}
